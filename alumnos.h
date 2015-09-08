e7ff7265d020de62aa55fe199bf662bc279709aa
yieeei <sebastianjorc@gmail.com>
2015-09-08 13:02:26 -0300

    Estructura|Martes

--- alumnos.h
+++ alumnos.h
@@ -10,36 +10,45 @@
 
 # define tam 8
 
-typedef struct tipo_curso{	int interaccion[tam][tam];  }Curso;
+typedef struct tipo_curso{	int interaccion[tam][tam];  Alumno alumnos[tam]}Curso;
 //Aquí sirve para la interacción, osea estatico.
-typedef struct tipo_alumno{	char letra;	int pos;}Alumno;
+typedef struct tipo_alumno{	char letra;	int prof, estado/*,pos*/;}Alumno;
 
 void inicializar_alumnos (Alumno AlumnoS[tam]){
-	AlumnoS[0]->letra='a'; Alumnos[0]->pos=0;
-	AlumnoS[1]->letra='b'; Alumnos[1]->pos=1;
-	AlumnoS[2]->letra='c'; Alumnos[2]->pos=2;
-	AlumnoS[3]->letra='d'; Alumnos[3]->pos=3;
-	AlumnoS[4]->letra='e'; Alumnos[4]->pos=4;
-	AlumnoS[5]->letra='f'; Alumnos[5]->pos=5;
-	AlumnoS[6]->letra='g'; Alumnos[6]->pos=6;
-	AlumnoS[7]->letra='h'; Alumnos[7]->pos=7;
-	AlumnoS[8]->letra='i'; Alumnos[8]->pos=8;
+	AlumnoS[0]->letra='a'; AlumnoS[0]=AlumnoS[0]=0;	//Alumnos[0]->pos=0;
+	AlumnoS[1]->letra='b'; AlumnoS[1]=AlumnoS[1]=0;	//Alumnos[1]->pos=1;
+	AlumnoS[2]->letra='c'; AlumnoS[2]=AlumnoS[2]=0;	//Alumnos[2]->pos=2;
+	AlumnoS[3]->letra='d'; AlumnoS[3]=AlumnoS[3]=0;	//Alumnos[3]->pos=3;
+	AlumnoS[4]->letra='e'; AlumnoS[4]=AlumnoS[4]=0;	//Alumnos[4]->pos=4;
+	AlumnoS[5]->letra='f'; AlumnoS[5]=AlumnoS[5]=0;	//Alumnos[5]->pos=5;
+	AlumnoS[6]->letra='g'; AlumnoS[6]=AlumnoS[6]=0;	//Alumnos[6]->pos=6;
+	AlumnoS[7]->letra='h'; AlumnoS[7]=AlumnoS[7]=0;	//Alumnos[7]->pos=7;
+	AlumnoS[8]->letra='i'; AlumnoS[8]=AlumnoS[8]=0;	//Alumnos[8]->pos=8;
 }
 
-Curso inicializar_curso (Alumno alumnoS[tam]){
-//			0.1.2.3.4.5.6.7.8
-	Curso curso={	0,1,1,1,1,0,0,0,0,/*0*/
-		     	1,0,1,0,0,0,0,0,0,/*1*/
-		     	1,1,0,0,0,0,0,0,0,/*2*/
-		     	0,0,0,0,1,1,0,0,0,/*3*/
-		     	0,0,1,1,0,1,0,0,0,/*4*/
-			0,0,0,1,1,0,0,0,0,/*5*/
-			0,0,0,0,0,0,0,1,1,/*6*/
-			0,0,0,0,0,0,1,0,1,/*7*/
-			0,0,0,0,0,0,1,1,0;}/*8*/
+Curso inicializar_curso (){
+	Alumno AlumnoS[tam]=inicializar_alumnos();
+
+//				0.1.2.3.4.5.6.7.8
+	Curso curso[tam][tam]={	0,1,1,1,1,0,0,0,0,/*0*/
+		     		1,0,1,0,0,0,0,0,0,/*1*/
+		     		1,1,0,0,0,0,0,0,0,/*2*/
+		     		0,0,0,0,1,1,0,0,0,/*3*/
+		     		0,0,1,1,0,1,0,0,0,/*4*/
+				0,0,0,1,1,0,0,0,0,/*5*/
+				0,0,0,0,0,0,0,1,1,/*6*/
+				0,0,0,0,0,0,1,0,1,/*7*/
+				0,0,0,0,0,0,1,1,0;}/*8*/
 }
 
-void recorrer_grafo(Alumno alumno){
+void recorrer_grafoBFS(Alumno alumno){
+	//Aquí introducir todo el algoritmo de recorrido 
+	//o bien, crear funciones para hacerlo por partes
+/*
+	
+	
+}
+void recorrer_grafoDFS(Alumno alumno){
 	//Aquí introducir todo el algoritmo de recorrido 
 	//o bien, crear funciones para hacerlo por partes
 	
--- main.c
+++ main.c
@@ -20,9 +20,34 @@ void introducción (){
 }
 
 main(){
+	int op,flag=1;
 	introducción();
-	Alumno AlumnoS[tam];
-	inicializar_alumnos(AlumnoS[tam]);
 	Curso curso=inicializar_curso(AlumnoS[tam])
-	recorrer_grafo(AlumnoS[3]);	
+	while (flag==1){
+		printf("Ingrese la forma para recorrer el Grafo (G):\n");
+		printf("BFS (1)\tDFS(2)");
+		scanf("%i",&op);getchar();
+		while (op!=1 && op!=2){
+		 	printf("Error, ingrese opción valida: ");
+			scanf("%i\n",&op);getchar();
+		}
+		if (op==1){
+			recorrer_grafoBFS(AlumnoS[3]);	
+		}
+		else if (op==2){
+			recorrer_grafoDFS(AlumnoS[3]);	
+		}
+		op=0;
+		Printf("Desea seguir usando el programa?\n");
+		printf("Si (1)\tNo(2)\t: ");
+		scanf("%i\n",&op);getchar();
+		while (op!=1 && op!=2){
+		 	printf("Error, ingrese opción valida: ");
+			scanf("%i\n",&op);getchar();
+		}
+		if (op==2){
+			flag=0;
+			printf("Gracias por usar este programa\n Adiós.\n");
+		}
+	}
 }
